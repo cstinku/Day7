e2f1c34f4385d621456909e5da3aa434c707cef3
cstinku <alokchamp.sharma@gmail.com>
2023-06-10 15:51:44 +0530

    sec

---
+++ StringComparision.java
@@ -0,0 +1,15 @@
+public class StringComparision {
+
+    public static void main(String[] args) {
+        String string1 = "Alok";
+        String string2 = "Sharma";
+
+        if(string1.equals(string2))
+        {
+            System.out.println("string is equal");
+        }
+        else
+        {
+            System.out.println("string not equal");
+        }
+    }
